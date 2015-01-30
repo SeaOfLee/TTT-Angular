5e27eee139ec014de0aa3f6bfa9569041617bdea
SeaOfLee <l.richardson1@gmail.com>
2015-01-29 22:30:52 -0800

    Add p1/p2 win functions to win conditions

--- ttt2.js
+++ ttt2.js
@@ -11,30 +11,38 @@ function checkWin() {
   // checks if horizontal rows are equal to each other and not equal to an empty string.
   if($scope.board[i][0] == $scope.board[i][1] && $scope.board[i][2] == $scope.board[i][0] && $scope.board[i][0] !== "" && $scope.board[i][0] == "X"){
     console.log("X win horizontal");
+    p1Wins();
   }
   else if ($scope.board[i][0] == $scope.board[i][1] && $scope.board[i][2] == $scope.board[i][0] && $scope.board[i][0] !== "" && $scope.board[i][0] == "O"){
     console.log("O win horizontal");
+    p2Wins();
   }
   // checks if vertical columns are equal to each other and not equal to an empty string.
   else if ($scope.board[0][i] == $scope.board[1][i] && $scope.board[2][i] == $scope.board[0][i] && $scope.board[0][i] !== "" && $scope.board[0][i] == "X") {
     console.log("X win vertical");
+    p1Wins();
   }
   else if ($scope.board[0][i] == $scope.board[1][i] && $scope.board[2][i] == $scope.board[0][i] && $scope.board[0][i] !== "" && $scope.board[0][i] == "O") {
     console.log("O win vertical");
+    p2Wins();
   // checks diagonal. only true when i = 0
   }
   else if ($scope.board[0][i] == $scope.board[1][i+1] && $scope.board[2][i +2] == $scope.board[0][i] && $scope.board[0][i] !== "" && $scope.board[0][i] == "X") {
     console.log("X win diagonal right");
+    p1Wins();
   }
   else if ($scope.board[0][i] == $scope.board[1][i+1] && $scope.board[2][i +2] == $scope.board[0][i] && $scope.board[0][i] !== "" && $scope.board[0][i] == "O") {
     console.log("O win diagonal right");
+    p2Wins();
   }
   //checks left diagonal. only true when i = 2
   else if ($scope.board[0][i] == $scope.board[1][i-1] && $scope.board[2][i-2] == $scope.board[0][i] && $scope.board[0][i] !== "" && $scope.board[0][i] == "X") {
     console.log("X win diagonal left");
+    p1Wins();
   }
   else if ($scope.board[0][i] == $scope.board[1][i-1] && $scope.board[2][i-2] == $scope.board[0][i] && $scope.board[0][i] !== "" && $scope.board[0][i] == "O") {
     console.log("O win diagonal left");
+    p2Wins();
   }
   // if turnNumber reaches 9 there's been no winner, tie condition is enforced.
   else if ($scope.turnNumber == 9) {
@@ -58,6 +66,20 @@ function checkWin() {
     console.log($scope.board);
 
   };
+
+  $scope.p1WinTotal = 0;
+  $scope.p2WinTotal = 0;
+
+  function p1Wins() {
+    alert("X Wins!");
+    $scope.turnNumber = 0;
+    $scope.p1WinTotal++;
+  }
+  function p2Wins() {
+    alert("O Wins!");
+    $scope.turnNumber = 0;
+    $scope.p2WinTotal++;
+  }
 
   // function clearBoxes() {
   //   for(i = 0;)
